b9a9840fa16f82f7d2d34fc6513b072028decb35
Nsanawaji <nsanawaji>
2022-10-13 13:29:24 -0700

    Print string with c using puts.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-int puts(const char *"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A C program that prints with put function
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
