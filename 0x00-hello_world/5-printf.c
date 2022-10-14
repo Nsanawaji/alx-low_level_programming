0ab20293188408421d4a0267bd89b5f6d10f6ac6
Nsanawaji <nsanawaji>
2022-10-14 02:13:13 -0700

    A C program that uses printf to print a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - This the entry point
+ *
+ * Description: A C program that uses printf function
+ *
+ * Return: Always 0 (success)
+ *
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
