1b1bc608bd21c2969997d7521c2cf3de851652df
Nsanawaji <nsanawaji>
2022-10-16 15:16:28 -0700

    Escaping characters in c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: Printing in C
+ *
+ * Return: Always zero (success)
+ *
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
