f1fa875ed89300c4e779cc80dc9fd35c5b1a2569
Nsanawaji <nsanawaji>
2022-10-17 07:04:44 -0700

    Printing lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		putchar('\n');
+		alpha++;
+	}
+		return (0);
+}
