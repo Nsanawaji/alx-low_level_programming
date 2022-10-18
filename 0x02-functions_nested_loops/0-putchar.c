46b4debaef5ba4a604a6fd2e8fa1455a09ce90e7
Nsanawaji <nsanawaji>
2022-10-18 07:02:53 -0700

    Correcting

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,16 +10,13 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	-putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+
 	_putchar('\n');
 
 	return (0);
-
 }
