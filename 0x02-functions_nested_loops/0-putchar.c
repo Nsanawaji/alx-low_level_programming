10742012d4d704d2578714b21e3b714cb68542d2
Nsanawaji <nsanawaji>
2022-10-18 06:36:58 -0700

    Printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Entry point of program
+ *
+ * Description: Printing _putchar
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	-putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
