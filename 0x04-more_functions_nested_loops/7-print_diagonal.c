417d5c1f3b4008720c1b9608636e910ef656789c
Nsanawaji <nsanawaji>
2022-10-21 16:02:14 -0700

    Print a square

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_diagonal - function that prints line
+*@n: specifies length of line
+*Return: 0
+*/
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
