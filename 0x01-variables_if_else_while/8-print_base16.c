15d48f4ba102c35c2821d1b522981e36f3f23588
Nsanawaji <nsanawaji>
2022-10-22 14:18:50 -0700

    Printing hexadecimals in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: Printing all the numbers of base 16 in lowercase
+ * Description: print numbers of base16 in lowercase
  *
- * Return: 0
- *
- */
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	int digit = 48; /*48; decimal rep of 0*/
@@ -18,14 +18,10 @@ int main(void)
 
 		/* after 9 we jump till 96; `*/
 		if (digit == 57)
-		{
 			digit += 39;
-		}
 		++digit;
 	}
-														}
 	putchar('\n');
 
 	return (0);
-	
 }
