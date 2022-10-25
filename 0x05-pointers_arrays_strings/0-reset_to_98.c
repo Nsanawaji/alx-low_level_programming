a529ea9f2cb6ebb61b6a9b0495773778b21e1acc
Nsanawaji <nsanawaji>
2022-10-25 10:51:23 -0700

    Using pointer to dereference

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*reset_to_98 - function that resets the value of a variable to 98 using its address
+*@n: Variable set as parameter
+*Return: Nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+        int x = 0;
+
+        while (++x)
+                printf("%d", x);
+	printf("\n");
+        return (0);
+}
