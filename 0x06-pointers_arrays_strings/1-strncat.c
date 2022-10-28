6c5d0c247778086d7ac98eb45d26622ff0f82fd0
Nsanawaji <nsanawaji>
2022-10-28 15:21:56 -0700

    Copying a string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_strncat - function that concatenates two strings
+*@dest: Destination
+*@src: Source
+*@n: Integer variable
+*Return: Return a character pointer
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	
+}
