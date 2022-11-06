660b8fb73ef60436f1f3003f238152ab8cb35c73
Nsanawaji <nsanawaji>
2022-11-06 13:59:11 -0800

    program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+*@argc: Counts the number of arguments
+*
+*@argv: Array of argument strings
+*
+*Return: 0 success
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
